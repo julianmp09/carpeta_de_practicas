80aba0e1cb08e861fb8ec7401cff6d17ab390456
Julian Domselaar <juliandaniel9809@gmail.com>
2024-05-28 17:53:29 -0300

    Añandiendo clase-3

---
+++ javascript-repaso/clase-3/ciclos-e-iteraciones.js
@@ -0,0 +1,63 @@
+/* 
+    Estructura del bucle for
+
+    for(desde; hasta; actualizacion){
+        codigo a repetir
+    }
+*/
+
+// i++ quiere decir esto => i = i + 1
+
+// for(let i = 0; i < 100; i++){
+//     console.log(i);
+// }
+
+// Crearemos un programa que me de la respesta de alguna tabla de multiplicacion
+
+// let numero = Number(prompt("Ingrese el numero: "))
+
+// for(let i=0; i <=10; i++){
+//     let resultado = i * numero;
+//     console.log(resultado);
+// }
+
+
+/* Suma de números pares: Escribe un programa que sume todos los números pares del 1 al 100 e imprima el resultado.
+ */
+/* for(let i = 0; i <= 100; i= i+2){
+    console.log(i);
+} */
+
+
+/* Imprimir números pares del 1 al 20: Escribe un programa que imprima los números pares del 1 al 20 en la consola. */
+
+
+// for(let i=0; i <=20; i= i+2){
+//     console.log(i);
+// }
+
+/* Suma de los primeros N números naturales: Escribe un programa que calcule la suma de los primeros N números naturales, donde N es un número dado por el usuario.
+ */
+// let sumaN = Number(prompt("Ingrese un numero:"));
+// let sumaTotal = 0;
+
+// for (let i = 1; i <= sumaN; i++) {
+//     sumaTotal = sumaTotal + i;
+// }
+
+// console.log(`La suma de los primeros ${sumaN} números naturales es: ${sumaTotal}`);
+
+/* Contar vocales en una cadena: Escribe un programa que cuente y muestre la cantidad de vocales (a, e, i, o, u) en una cadena dada por el usuario.
+ */
+
+let cadena = prompt("Ingrese una cadena de texto:").toLowerCase(); // Convertimos la cadena a minúsculas para hacer la comparación de manera no sensible a mayúsculas/minúsculas
+let conteoVocales = 0;
+
+for (let i = 0; i < cadena.length; i++) {
+    let caracter = cadena[i];
+    if (caracter === 'a' || caracter === 'e' || caracter === 'i' || caracter === 'o' || caracter === 'u') {
+        conteoVocales++;
+    }
+}
+
+console.log(`La cantidad de vocales en la cadena "${cadena}" es: ${conteoVocales}`);
